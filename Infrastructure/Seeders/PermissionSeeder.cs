7bfafb3b4b58d14c0bdc584ffcc9c8f23cd667b8
Daniel Prastha <danielprastha4@gmail.com>
2024-10-23 15:38:39 +0700

    feat: add role & permission seeder

---
+++ Infrastructure/Seeders/PermissionSeeder.cs
@@ -0,0 +1,43 @@
+using System.Text.Json;
+using DotNetService.Infrastructure.Helpers;
+using DotNetService.Models;
+
+namespace DotNetService.Infrastructure.Seeders
+{
+  public class PermissionSeeder : ISeeder
+  {
+    public async Task Seed(IamDBContext dbContext, ILogger logger)
+    {
+      logger.LogInformation("Seeding Permissions...");
+      var jsonPath = "Seeders/Permission.json";
+
+      var jsonString = await File.ReadAllTextAsync(jsonPath);
+
+      var permissions = JsonSerializer.Deserialize<List<Permission>>(jsonString, JsonSerializeSeeder.options);
+      var newPermissions = new List<Permission>();
+
+      try
+      {
+        await dbContext.Database.BeginTransactionAsync();
+
+        foreach (var permission in permissions)
+        {
+          permission.Name = permission.Name;
+          permission.Key = permission.Key;
+
+          newPermissions.Add(permission);
+        }
+        await dbContext.Permissions.AddRangeAsync(newPermissions);
+        await dbContext.SaveChangesAsync();
+        await dbContext.Database.CommitTransactionAsync();
+      }
+      catch (Exception e)
+      {
+        logger.LogError(e, "Error while seeding Permissions");
+        await dbContext.Database.RollbackTransactionAsync();
+      }
+
+      logger.LogInformation("Seeding Permissions complete");
+    }
+  }
+}
