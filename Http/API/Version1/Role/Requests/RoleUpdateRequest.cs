95d7368c54aa3b9f632d9c27324dd8d25d0314c5
Daniel Prastha <danielprastha4@gmail.com>
2024-10-29 13:28:06 +0700

    rollback: test file

--- Http/API/Version1/Role/Requests/RoleUpdateRequest.cs
+++ Http/API/Version1/Role/Requests/RoleUpdateRequest.cs
@@ -13,9 +13,6 @@ namespace DotNetService.Http.API.Version1.Role.Requests
         [JsonPropertyName("permission_ids")]
         public List<Guid> PermissionIds { get; set; }
 
-        // [JsonPropertyName("permission_ids")]
-        // public List<Guid> PermissionIds { get; set; }
-
         public static Models.Role Assign(RoleUpdateRequest data)
         {
             Models.Role res = new()
