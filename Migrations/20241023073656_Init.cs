37392751c093f58d39ca6e5eeab0df6ca580392f
Daniel Prastha <danielprastha4@gmail.com>
2024-10-29 09:24:46 +0700

    test sonarqube config

--- Migrations/20241023073656_Init.cs
+++ Migrations/20241023073656_Init.cs
@@ -13,7 +13,7 @@ namespace DotNetService.Migrations
                 name: "permissions",
                 columns: table => new
                 {
-                    // id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
                     // name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     // key = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     // created_at = table.Column<DateTime>(type: "datetime2", nullable: true),
