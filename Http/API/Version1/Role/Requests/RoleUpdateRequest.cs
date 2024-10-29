6ea909a135b3fb11493402f99a340dbb47356f67
Daniel Prastha <danielprastha4@gmail.com>
2024-10-29 09:30:11 +0700

    test sonarqube config

--- Http/API/Version1/Role/Requests/RoleUpdateRequest.cs
+++ Http/API/Version1/Role/Requests/RoleUpdateRequest.cs
@@ -13,6 +13,9 @@ namespace DotNetService.Http.API.Version1.Role.Requests
         [JsonPropertyName("permission_ids")]
         public List<Guid> PermissionIds { get; set; }
 
+        // [JsonPropertyName("permission_ids")]
+        // public List<Guid> PermissionIds { get; set; }
+
         public static Models.Role Assign(RoleUpdateRequest data)
         {
             Models.Role res = new()
